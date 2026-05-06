c153b7031b3f8a9cf6ec8b041c417567ca558b0d
bardapraca <trip.marcio09@gmail.com>
2026-05-06 20:20:19 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Ignora os erros chatos de tipagem do TypeScript na hora de publicar
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Ignora os avisos de formatação na hora de publicar
     ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
